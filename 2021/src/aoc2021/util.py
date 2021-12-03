91fcabf05b7aae8ed73a60d09039c8f7b4669af7
CarlosLes <CarlosLes@users.noreply.github.com>
2021-12-03 09:53:29 +0000

    Add further type hint details for read_as_list

--- 2021/src/aoc2021/util.py
+++ 2021/src/aoc2021/util.py
@@ -1,10 +1,10 @@
 """Shared utilities."""
 
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Any
 
 
-def read_as_list(fpath: Path, func: Callable = str) -> list:
+def read_as_list(fpath: Path, func: Callable[[str], Any] = str) -> list:
     """Return file contents as list of lines mapped using `func`."""
     with open(fpath, 'r') as f:
         return [func(line) for line in f]
