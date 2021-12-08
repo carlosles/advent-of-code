30c4bd544e3564cfac77d223361a9c036c0fde5a
CarlosLes <CarlosLes@users.noreply.github.com>
2021-12-08 16:06:05 +0000

    Add dictionary invert function to aoc2021

--- 2021/src/aoc2021/util.py
+++ 2021/src/aoc2021/util.py
@@ -13,3 +13,8 @@ def read_as_list(fpath: Path, func: Callable[[str], Any] = str) -> list:
 def chunks(xs: list, n: int) -> list[list]:
     """Yield successive n-sized chunks from list."""
     return [xs[i:i+n] for i in range(0, len(xs), n)]
+
+
+def invert_dict(d: dict[Any, Any]) -> dict[Any, Any]:
+    """Return dictionary with keys and values inverted."""
+    return {val: key for key, val in d.items()}
