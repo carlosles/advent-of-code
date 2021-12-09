a205d394b9010ebb1dec54fd994d66a626c10ad7
CarlosLes <CarlosLes@users.noreply.github.com>
2021-12-09 15:19:32 +0000

    Add various functions for nested lists

--- 2021/src/aoc2021/util.py
+++ 2021/src/aoc2021/util.py
@@ -18,3 +18,20 @@ def chunks(xs: list, n: int) -> list[list]:
 def invert_dict(d: dict[Any, Any]) -> dict[Any, Any]:
     """Return dictionary with keys and values inverted."""
     return {val: key for key, val in d.items()}
+
+
+def transpose(xss: list[list[Any]]) -> list[list[Any]]:
+    """Return shortest full transposed list of lists."""
+    return list(map(list, zip(*xss)))
+
+
+def is_rectangular(xss: list[list[Any]]) -> bool:
+    """Return true if list of lists is rectangular."""
+    return len(set(map(len, xss))) == 1
+
+
+def pad(xss: list[list[Any]], fill: Any) -> list[list[Any]]:
+    """Return rectangular list of lists with border padding."""
+    assert is_rectangular(xss), 'list of lists is not rectangular'
+    n = len(xss[0]) + 2
+    return [[fill]*n] + [[fill]+xs+[fill] for xs in xss] + [[fill]*n]
