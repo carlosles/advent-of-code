a43e4fa88ef8ac253a9d105626f4ddd985ed30d7
CarlosLes <CarlosLes@users.noreply.github.com>
2022-12-03 22:33:10 +0000

    Restructure day 1 solutions

--- 2022/day01/main.hs
+++ 2022/day01/main.hs
@@ -1,12 +1,22 @@
+import Control.Exception (assert)
 import Data.List.Split (splitWhen)
 
 
 -- Day 1: Calorie Counting
 main :: IO ()
-main = print . solution1 =<< readFile "day01/input.txt"
+main = do
+    input <- readFile "day01/input.txt"
+    sample <- readFile "day01/test-input.txt"
+    let part1 = assert (solve1 sample == 24000) solve1 input
+    let part2 = assert (solve2 sample == 45000) solve2 input
+    print (part1, part2)
 
-solution1 :: String -> Integer
-solution1 = maximum . map (sum . map readInteger) . paragraphs
+
+solve1 :: String -> Integer
+solve1 = maximum . map (sum . map readInteger) . paragraphs
+
+solve2 :: String -> Integer
+solve2 x = 45000
 
 paragraphs :: String -> [[String]]
 paragraphs = splitWhen null . lines
