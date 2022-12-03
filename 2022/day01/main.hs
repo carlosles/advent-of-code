2bca846d1eb6422ac0c371c275f16e00ac958a15
CarlosLes <CarlosLes@users.noreply.github.com>
2022-12-03 23:02:20 +0000

    Simplify file naming

--- 2022/day01/main.hs
+++ 2022/day01/main.hs
@@ -6,8 +6,8 @@ import Data.List.Split (splitWhen)
 -- Day 1: Calorie Counting
 main :: IO ()
 main = do
-    input <- readFile "day01/input.txt"
-    sample <- readFile "day01/test-input.txt"
+    input <- readFile "input.txt"
+    sample <- readFile "sample.txt"
     let part1 = assert (solve1 sample == 24000) solve1 input
     let part2 = assert (solve2 sample == 45000) solve2 input
     print (part1, part2)
