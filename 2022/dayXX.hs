45b01f03347a8a04ceb20046ee52a81a85ae635c
CarlosLes <CarlosLes@users.noreply.github.com>
2022-12-03 23:03:06 +0000

    Add a template Haskell file for solving the day's challenge

---
+++ 2022/dayXX.hs
@@ -0,0 +1,20 @@
+import Control.Exception (assert)
+import Data.List (sort)
+import Data.List.Split (splitWhen)
+
+
+-- Day XX: Title
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    sample <- readFile "sample.txt"
+    let part1 = assert (solve1 sample == 100) solve1 input
+    let part2 = assert (solve2 sample == 200) solve2 input
+    print (part1, part2)
+
+
+solve1 :: String -> Integer
+solve1 x = 100
+
+solve2 :: String -> Integer
+solve2 x = 200
